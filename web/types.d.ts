4119997ddd94198e4fb99900dc214180c679219c
Joseph Abbey <me@josephabbey.dev>
2024-01-31 10:12:49 +0000

    Documentation for web editor

--- web/types.d.ts
+++ web/types.d.ts
@@ -1,3 +1,7 @@
 declare namespace json {
   export function parse(text: string): import('json-ast-comments').JsonDocument;
 }
+
+declare module 'https://cdn.jsdelivr.net/npm/monaco-yaml@5.1.1/+esm' {
+  export * from 'monaco-yaml';
+}
