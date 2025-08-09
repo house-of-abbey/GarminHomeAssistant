e9f6ccfd0d365c93f1f7685aab3cc1e929ce9185
Philip Abbey <philipabbey@users.noreply.github.com>
2025-08-09 13:42:55 +0100

    Updated translations from @tispokes
    Amended translate.py and created a batch script to ease of running the translations.
    
    Co-Authored-By: Lars Pöpperl <45465820+tispokes@users.noreply.github.com>

---
+++ translate.cmd
@@ -0,0 +1,24 @@
+@echo off
+rem -----------------------------------------------------------------------------------
+rem
+rem  Distributed under MIT Licence
+rem    See https://github.com/house-of-abbey/GarminHomeAssistant/blob/main/LICENSE.
+rem
+rem -----------------------------------------------------------------------------------
+rem
+rem  GarminHomeAssistant is a Garmin IQ application written in Monkey C and routinely
+rem  tested on a Venu 2 device. The source code is provided at:
+rem             https://github.com/house-of-abbey/GarminHomeAssistant.
+rem
+rem  J D Abbey & P A Abbey, 28 December 2022
+rem
+rem  Run the automatic translation script.
+rem
+rem  Reference:
+rem   * Using Monkey C from the Command Line
+rem     https://developer.garmin.com/connect-iq/reference-guides/monkey-c-command-line-setup/
+rem
+rem -----------------------------------------------------------------------------------
+
+python translate.py
+pause
