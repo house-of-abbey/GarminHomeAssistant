a30f0c621821612258dc3aea4800036ce48ed717
Philip Abbey <philipabbey@users.noreply.github.com>
2026-01-25 17:46:09 +0000

    Bulk amendment of header comments.
    Removed "." from the end of all URLs because Copilot complains it can't find the URLs and its too stupid to realise the "." should be omitted. E.g. "https://github.com/house-of-abbey/GarminHomeAssistant/blob/main/LICENSE."

--- translate.cmd
+++ translate.cmd
@@ -2,13 +2,13 @@
 rem -----------------------------------------------------------------------------------
 rem
 rem  Distributed under MIT Licence
-rem    See https://github.com/house-of-abbey/GarminHomeAssistant/blob/main/LICENSE.
+rem    See https://github.com/house-of-abbey/GarminHomeAssistant/blob/main/LICENSE
 rem
 rem -----------------------------------------------------------------------------------
 rem
 rem  GarminHomeAssistant is a Garmin IQ application written in Monkey C and routinely
 rem  tested on a Venu 2 device. The source code is provided at:
-rem             https://github.com/house-of-abbey/GarminHomeAssistant.
+rem             https://github.com/house-of-abbey/GarminHomeAssistant
 rem
 rem  J D Abbey & P A Abbey, 28 December 2022
 rem
